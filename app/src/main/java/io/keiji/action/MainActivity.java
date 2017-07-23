abd103d1f785fe663a274fdfaf30ebf65101db62
渡邊　海斗 <kaito@S-watanabekaito-no-MacBook-Air.local>
2017-07-23 21:53:24 +0900

    ゲームオーバー後にタイトル画面に移動

--- app/src/main/java/io/keiji/action/MainActivity.java
+++ app/src/main/java/io/keiji/action/MainActivity.java
@@ -1,5 +1,6 @@
 package io.keiji.action;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onGameOver(){
         Toast.makeText(this,"Game Over",Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(this,StartActivity.class);
+        startActivity(intent);
     }
 
     @Override
